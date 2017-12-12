89155948de91c0aed2068a4aaeacbc8a58f2d362
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-12-12 20:55:17 -0200

    criado primeiro teste

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -6,9 +6,12 @@ namespace test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void VerificaVendedor()
         {
-
+            PPT ppt = new PPT("João", "Marcos");
+            ppt.jogador1_material = "Papel";
+            ppt.jogador2_material = "Pedra";            
+            Assert.Equal(ppt.vencedor, "João");
         }
     }
 }
